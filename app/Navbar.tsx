e39cfbba6a9b0abfe385cc3338ab75a5cda33b37
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-10-21 16:08:05 -0400

    added classnames package to render dynamic classes

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,15 @@
+'use client'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames'
 
 const Navbar = () => {
+
+    const currentPath = usePathname();
+    
+
     const links = [
         {label: "Dashboard", href: "/"},
         {label: "Issues", href: "/issues"},
@@ -12,7 +19,11 @@ const Navbar = () => {
         <Link href="/"><AiFillBug /></Link>
         <ul className='flex space-x-6'>
             {links.map((link) => (
-                <li key={link.label}><Link href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-color'>{link.label}</Link></li>
+                <li key={link.label}><Link href={link.href} className={classNames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+                })}>{link.label}</Link></li>
             ))}
         </ul>
     </nav>
