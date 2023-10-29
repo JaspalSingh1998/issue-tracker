854de6c325125d014222724d7c9ceb256b5c4b89
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-10-29 09:45:39 -0400

    Enhanced api to assigne the issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -9,12 +9,24 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(authOptions);
-  if(!session) return NextResponse.json({}, {status: 401});
+  if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { title, description, assignedToUserId } = body;
+  if (assignedToUserId) {
+    const user = await prisma.issue.findUnique({
+      where: {
+        id: assignedToUserId,
+      },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -29,8 +41,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title: title,
+      description: description,
+      assignedToUserId
     },
   });
 
@@ -42,7 +55,7 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(authOptions);
-  if(!session) return NextResponse.json({}, {status: 401});
+  if (!session) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
